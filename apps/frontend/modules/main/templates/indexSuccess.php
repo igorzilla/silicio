d3e3406d41233cca1d3271db31644251c1e347eb
Pedro Bonilla <pedroabp5@gmail.com>
2010-01-06 20:14:46 -0500

    Cambio del nombre del módulo 'principal' por 'main'. Traducción del nombre de todas las variables a inglés

---
+++ apps/frontend/modules/main/templates/indexSuccess.php
@@ -0,0 +1,17 @@
+<script>
+Ext.onReady(function() {
+	buildWorkArea();
+});
+
+</script>
+<div id="paintarea"
+	style="position: relative; width: 3000px; height: 3000px;"></div>
+<div id="AND_cover" class="gates_cover">
+<div id="AND" class="gate"><?php echo image_tag('AND.gif');?></div>
+</div>
+<div id="OR_cover" class="gates_cover">
+<div id="OR" class="gate"><?php echo image_tag('OR.gif');?></div>
+</div>
+<div id="AND_cover" class="gates_cover">
+<div id="NOT" class="gate"><?php echo image_tag('NOT.gif');?></div>
+</div>
---
+++ web/js/main.js
@@ -0,0 +1,96 @@
+function buildWorkArea(){
+    var workflow = new draw2d.Workflow("paintarea");
+    workflow.setBackgroundImage("/images/grid_10.png", true);
+    var gates = new Ext.Panel({
+        region: 'west',
+        xtype: 'panel',
+        split: true,
+        width: 200,
+        collapsible: true,
+        collapseMode: 'mini',
+        minSize: 200,
+        title: 'Compuertas'
+    });
+    var viewport = new Ext.Viewport({
+        layout: 'border',
+        renderTo: Ext.getBody(),
+        items: [{
+            region: 'north',
+            xtype: 'panel',
+            titlebar: true,
+            title: 'SILICIO',
+            height: 53,
+            items: [{
+                xtype: 'toolbar',
+                items: [{
+                    xtype: 'button',
+                    text: 'Archivo',
+                    menu: [{
+                        text: 'Nuevo diseño'
+                    }, {
+                        text: 'Abrir'
+                    }, {
+                        text: 'Guardar'
+                    }, {
+                        text: 'Cerrar sesión'
+                    }]
+                }, '-', {
+                    xtype: 'button',
+                    text: 'Editar',
+                    menu: [{
+                        text: 'Deshacer'
+                    }, {
+                        text: 'Rehacer'
+                    }, {
+                        text: 'Cortar'
+                    }, {
+                        text: 'Copiar'
+                    }, {
+                        text: 'Pegar'
+                    }]
+                }, '-', {
+                    xtype: 'button',
+                    text: 'Ayuda',
+                    menu: [{
+                        text: 'Contenido'
+                    }, {
+                        text: 'Acerca de...'
+                    }]
+                }]
+            }]
+        }, gates, {
+            region: 'center',
+            xtype: 'panel',
+            contentEl: "paintarea"
+        }]
+    });
+    gates.add({
+        contentEl: 'AND_cover',
+        width: 200,
+        height: 100,
+        border: false
+    });
+//    workflow.scrollArea = document.getElementById("paintarea").parentNode;
+    new Ext.dd.DragSource("AND");
+    new Ext.dd.DropTarget("paintarea", {
+        notifyDrop: function(source, event, data){
+            workflow.addFigure(new draw2d.ImageFigure("/images/AND.gif"), event.xy[0] - workflow.getAbsoluteX(), event.xy[1] - workflow.getAbsoluteY());
+            return true;
+        }
+    });
+    gates.add({
+        xtype: 'panel',
+        contentEl: 'OR',
+        width: 200,
+        height: 100,
+        border: false
+    });
+    gates.add({
+        xtype: 'panel',
+        contentEl: 'NOT',
+        width: 200,
+        height: 100,
+        border: false
+    });
+    gates.doLayout();
+}
